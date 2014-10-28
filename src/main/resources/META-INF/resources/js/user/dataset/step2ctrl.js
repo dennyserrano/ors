735fd5831bfee95eb293f673aeb34cf87a77a0df
Psycotrompus <ejlayco@gmail.com>
2014-10-28 17:01:45 +0800

    step 2 - select all checkbox per table/dataset now working and monitors selection state.

--- src/main/resources/META-INF/resources/js/user/dataset/step2ctrl.js
+++ src/main/resources/META-INF/resources/js/user/dataset/step2ctrl.js
@@ -3,39 +3,49 @@
 angular.module('UserApp')
     .controller('Step2Ctrl', ['$scope', 'UserDatasetService',
         function($scope, UserDatasetService) {
+            
             $scope.step1 = 'complete';
             $scope.step2 = 'active';
             $scope.step3 = 'disabled';
             $scope.step4 = 'disabled';
+            
+            $scope.allElementsSelected = {};
 
-            $scope.elements = {};
+            $scope.selectedElements = {};
             UserDatasetService.get({}, function(dataset) {
                 $scope.dataset = dataset;
                 angular.forEach(dataset.subDatasets, function(subdataset) {
-                    $scope.elements[subdataset.id] = [];
-                    angular.forEach(subdataset.elements, function(element) {
-                        $scope.elements[subdataset.id].push(element);
-                    });
+                    $scope.allElementsSelected[subdataset.id] = false;
+                    $scope.selectedElements[subdataset.id] = [];
+                    $scope.selectAllElements(subdataset);
                 });
             });
             
             $scope.elementSelected = function(subdataset, element) {
-                var selectedElements = $scope.elements[subdataset.id];
+                var selected = false;
+                var selectedElements = $scope.selectedElements[subdataset.id];
                 angular.forEach(selectedElements, function(selectedElement) {
                     if (element.id === selectedElement.id) {
-                        return true;
+                        selected = true;
                     }
                 });
-                return false;
+                $scope.allElementsSelected[subdataset.id] = $scope.selectedElements[subdataset.id].length == subdataset.elements.length;
+                return selected;
             };
 
             $scope.selectAllElements = function(subdataset) {
-                $scope.elements[subdataset.id] = [];
-                angular.forEach(subdataset.elements, function(element) {
-                    if (!$scope.elementSelected(subdataset, element)) {
-                        $scope.elements[subdataset.id].push(element);
-                    }
-                });
+                var result = !$scope.allElementsSelected[subdataset.id];
+                if (!result) {
+                    $scope.selectedElements[subdataset.id] = [];
+                }
+                else {
+                    angular.forEach(subdataset.elements, function(element) {
+                        if (!$scope.elementSelected(subdataset, element)) {
+                            $scope.selectedElements[subdataset.id].push(element);
+                        }
+                    });
+                }
+                $scope.allElementsSelected[subdataset.id] = result;
             };
         }
     ]
