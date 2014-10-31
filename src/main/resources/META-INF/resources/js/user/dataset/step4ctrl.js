b6af951cff13c74e5dedd10cf362ebcfa3db071c
Psycotrompus <ejlayco@gmail.com>
2014-10-31 11:48:53 +0800

    Step4 page with "Export As" button offering multiple output/download types

--- src/main/resources/META-INF/resources/js/user/dataset/step4ctrl.js
+++ src/main/resources/META-INF/resources/js/user/dataset/step4ctrl.js
@@ -1,14 +1,33 @@
 'use strict';
 
 angular.module('UserApp')
-    .controller('Step4Ctrl', ['$scope', 'UserDatasetService',
-        function($scope, UserDatasetService) {
+    .controller('Step4Ctrl', ['$scope', 'UserDatasetService', 'PreviewDataService',
+        function($scope, UserDatasetService, PreviewDataService) {
 
             $scope.step1 = 'complete';
             $scope.step2 = 'complete';
             $scope.step3 = 'complete';
             $scope.step4 = 'active';
             
+            $scope.loadingData = true;
+            $scope.datas = [];
+            
+            var previewDataCallback = function(data) {
+                $scope.headers = data[0];
+                data.splice(0, 1); // removes the header
+                $scope.datas = angular.copy(data);
+                $scope.loadingData = false;
+            };
+            
+            UserDatasetService.get({}, function(dataset) {
+                $scope.dataset = dataset;
+                $scope.datasetJson = angular.toJson(dataset);
+                PreviewDataService(dataset, previewDataCallback);
+            });
+            
+            $scope.export = function() {
+                $('#exportForm').submit();
+            };
         }
     ]
 );
