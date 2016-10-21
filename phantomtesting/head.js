550319a5db6d8591c3ac2b885c749228f877ed06
Denny <denny.serrano87@gmail.com>
2016-10-21 17:50:07 +0800

    phantom test

---
+++ phantomtesting/head.js
@@ -0,0 +1,5 @@
+var test=require('traverser.js');
+var casper=require('casper');
+
+var runner=test.runner(casper);
+runner.start();
---
+++ phantomtesting/traverser.js
@@ -0,0 +1,118 @@
+var casper=require('casper').create({
+	verbose:true,
+	logLevel:'debug'
+});
+
+
+var URL_BASE="http://localhost:8080/";
+
+var li=function(resource,C){
+	
+	
+//	console.log(resource.url.indexOf('preview')!=-1);
+	if(resource.url.indexOf('preview')!=-1)
+	{
+		this.echo("REQUESTS:"+resource.url);
+	};
+	
+};
+
+casper.on("resource.requested", li);
+casper.on("resource.received",function(resource){
+//	if(JSON.stringify(obj).contains('Internal Server Error'))
+//		{
+//		this.echo("goccha!");
+//		}
+	
+	if(resource.url.indexOf('preview')!=-1)
+	{
+		this.echo(resource.statusText);
+	}
+});
+//casper.options.onResourceReceived=function(obj){
+//	this.echo("dataset:"+JSON.stringify(obj));
+//};
+
+casper.on('remote.message', function(message) {
+    this.echo("REMOTE MESSAGE: "+message);
+});
+
+
+
+//var f=new Function('test',"console.log('dynamic function')");
+//f();
+
+
+casperStart(casper);
+
+function casperStart(casper,next)
+{
+	
+	console.log("Starting.....");
+	
+	casper.start(URL_BASE+"user/datasets",function(){
+		
+		
+		this.evaluate(function(){
+			$("#chk8").click();
+			
+		});
+
+		
+		
+	});
+
+	casper.waitForSelector("h4.list-group-item-heading",function(){
+		this.echo("list group shown");
+		this.evaluate(function(){
+			
+			$(".btn-primary").click();
+			
+		});
+	});
+
+	casper.waitForUrl(URL_BASE+"user/datasets#/2",function(){
+		
+	});
+
+	casper.waitForSelector("thead",function(){
+		this.evaluate(function(){
+			$("#nextBtn").click();
+		});
+	})
+
+	casper.waitForUrl(URL_BASE+"user/datasets#/3",function(){
+		
+	});
+
+	casper.waitForSelector(".form-group",function(){
+		this.echo("list form group shown");
+		this.evaluate(function(){
+			
+			$(".btn-primary").click();
+			
+		});
+	});
+
+	casper.waitForUrl(URL_BASE+"user/datasets#/4",function(){
+		
+	});
+
+	casper.waitForSelector("gggg",function(){
+		
+		
+		
+	},function(){
+		
+		if(next)
+		{
+			console.log("next!");
+		}	
+		
+	},5000);
+
+	
+	
+
+	casper.run();
+}
