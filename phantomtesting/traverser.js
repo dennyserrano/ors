31ecd4a9f3f1992cb412abaeb557e8cb2674acaa
Denny <denny.serrano87@gmail.com>
2016-10-23 22:52:39 +0800

    updates

--- phantomtesting/traverser.js
+++ phantomtesting/traverser.js
@@ -3,6 +3,7 @@ var casper=require('casper').create({
 	logLevel:'debug'
 });
 
+//var casper2=require('casper').create();
 
 var URL_BASE="http://localhost:8080/";
 
@@ -37,125 +38,88 @@ casper.on('remote.message', function(message) {
     this.echo("REMOTE MESSAGE: "+message);
 });
 
+var html=[];
 
-var checkboxName='#chk8';
-
-
-var step1="casper.start(URL_BASE+'user/datasets',function(){" +
-		"this.evaluate(function(){" +
-		"$('"+checkboxName+"').click();"+
-		"});"+
-		"});";
-
-var step1WaitForSelector="casper.waitForSelector('h4.list-group-item-heading',function(){" +
-						"this.echo('list group shown');" +
-							"this.evaluate(function(){" +
-								"$('.btn-primary').click();" +
-							"});" +
-						"},10000);";
-
-var step2="casper.waitForUrl(URL_BASE+'user/datasets#/2',function(){" +
-		  "});";
+casper.start(URL_BASE+"user/datasets", function() {
+    html=this.evaluate(function() {
+    	var p=[];
+    	$(":checkbox").each(function(index,obj){
+    		p.push(obj.id);
+    	});
+    	return p;
+    });
+    
+    
+    
+    
+    
+    console.log(html);
+});
 
 
-var step2WaitSelector="casper.waitForSelector('thead',function(){" +
-							  "this.evaluate(function(){" +
-							  		"$('#nextBtn').click();" +
-							  "});" +
-					  "});";
-
-var step3="casper.waitForUrl(URL_BASE+'user/datasets#/3',function(){" +
-		  "});";
-
-var step3WaitSelector="casper.waitForSelector('.form-group',function(){" +
-							"this.echo('list form group shown');" +
-							"this.evaluate(function(){" +
-								"$('.btn-primary').click();" +
-							"});" +
-					  "},20000);";
-
-var step4="casper.waitForUrl(URL_BASE+'user/datasets#/4',function(){" +
-		"});";
+casper.run();
 
 
-var run="casper.run();";
+//while(html.length==0)
+//{
+//	;
+//}
+//console.log('ohoy');
+//var str=['chk8'];
+//
+//casper.start().eachThen(str,function(str){
+//	
+//	casper.thenOpenAndEvaluate(URL_BASE+"user/datasets",function(checkbox){
+//		console.log("about to click:"+checkbox);
+//		
+//		$("#"+checkbox).click();
+//	},str.data);
+//	
+//	
+//	casper.waitForSelector("h4.list-group-item-heading",function(){
+//		this.echo("list group shown");
+//		this.evaluate(function(){
+//			
+//			$(".btn-primary").click();
+//			
+//		});
+//	});
+//	
+//	
+//		
+//	casper.waitForUrl(URL_BASE+"user/datasets#/2",function(){
+//		
+//	});
+//	
+//	casper.waitForSelector("thead",function(){
+//		this.evaluate(function(){
+//			$("#nextBtn").click();
+//		});
+//	});
+//	
+//	casper.waitForUrl(URL_BASE+"user/datasets#/3",function(){
+//		
+//	});
+//	
+//	casper.waitForSelector(".form-group",function(){
+//		this.echo("list form group shown");
+//		this.evaluate(function(){
+//			
+//			$(".btn-primary").click();
+//			
+//		});
+//	});
+//	
+//	casper.waitForUrl(URL_BASE+"user/datasets#/4",function(){
+//		this.echo("open page 4!");
+//	});
+//	
+//	casper.open(URL_BASE+"user/datasets",function(){
+//		this.echo("open again page 1");
+//	});
+//	
+//	
+//
+//}).run();
 
-var casperString=step1+step1WaitForSelector+step2+step2WaitSelector+step3+step3WaitSelector+step4+run;
 
-var f=new Function('test',casperString);
-f();
-
-
-//casperStart(casper);
-
-function casperStart(casper,next)
-{
-	
-	console.log("Starting.....");
-	
-	casper.start(URL_BASE+"user/datasets",function(){
-		
-		
-		this.evaluate(function(){
-			$("#chk8").click();
-			
-		});
-
-		
-		
-	});
-
-	casper.waitForSelector("h4.list-group-item-heading",function(){
-		this.echo("list group shown");
-		this.evaluate(function(){
-			
-			$(".btn-primary").click();
-			
-		});
-	});
-
-	casper.waitForUrl(URL_BASE+"user/datasets#/2",function(){
-		
-	});
-
-	casper.waitForSelector("thead",function(){
-		this.evaluate(function(){
-			$("#nextBtn").click();
-		});
-	})
-
-	casper.waitForUrl(URL_BASE+"user/datasets#/3",function(){
-		
-	});
-
-	casper.waitForSelector(".form-group",function(){
-		this.echo("list form group shown");
-		this.evaluate(function(){
-			
-			$(".btn-primary").click();
-			
-		});
-	});
-
-	casper.waitForUrl(URL_BASE+"user/datasets#/4",function(){
-		
-	});
-
-	casper.waitForSelector("gggg",function(){
-		
-		
-		
-	},function(){
-		
-		if(next)
-		{
-			console.log("next!");
-		}	
-		
-	},5000);
-
-	
-	
-
-	casper.run();
-}
