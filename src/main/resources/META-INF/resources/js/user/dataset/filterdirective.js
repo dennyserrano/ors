0bb85888b79d68af5d2c2ffb59c7c0ec25757a13
Denny <denny.serrano87@gmail.com>
2016-11-22 16:11:14 +0800

    filter directive init

---
+++ src/main/resources/META-INF/resources/js/user/dataset/filterdirective.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('UserApp').directive('filterDirective',function(){
+
+	function link(scope, element, attrs) 
+	{
+	};
+
+
+	return {
+	
+		restrict:'E',
+		templateUrl:'',
+		scope:'',
+		link:''
+	
+	};
+
+});
