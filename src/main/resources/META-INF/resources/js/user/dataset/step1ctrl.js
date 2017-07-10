e00e62b736adbfc224469df56b09bb21371b9503
Denny <denny.serrano87@gmail.com>
2017-07-10 14:32:37 +0800

    c

--- src/main/resources/META-INF/resources/js/user/dataset/step1ctrl.js
+++ src/main/resources/META-INF/resources/js/user/dataset/step1ctrl.js
@@ -18,7 +18,10 @@ angular.module('UserApp')
             $scope.step3 = 'disabled';
             $scope.step4 = 'disabled';
             
+<<<<<<< cc26b6b83670576360ef22070ecf719766d9d652
             $window.ORS.AdjustDatasetContents(0);
+=======
+>>>>>>> changes
 
             DatasetService.query(function(datasets) {
                 $scope.datasets = datasets;
