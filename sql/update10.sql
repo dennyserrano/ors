38484a2e548a2bd27c5bf8258f305bf9966bb138
Denny <denny.serrano87@gmail.com>
2017-07-10 14:01:43 +0800

    changes

--- sql/update10.sql
+++ sql/update10.sql
@@ -1,4 +1,6 @@
-SELECT * FROM orsdb.dataset_correlation_group_dtl;CREATE TABLE `dataset_correlation_group_dtl` (
+
+CREATE TABLE `dataset_correlation_group_dtl` (
+
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `dataset_correlation_group_id` int(11) NOT NULL,
   `dataset_correlation_id` int(11) NOT NULL,
