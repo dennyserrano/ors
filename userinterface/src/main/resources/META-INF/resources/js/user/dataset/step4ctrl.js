f93d20949aea17f2a462d840b0efa91ceb244dd4
Denny <denny.serrano87@gmail.com>
2017-12-21 11:12:01 +0800

    validate mail

--- userinterface/src/main/resources/META-INF/resources/js/user/dataset/step4ctrl.js
+++ userinterface/src/main/resources/META-INF/resources/js/user/dataset/step4ctrl.js
@@ -87,6 +87,10 @@ angular.module('UserApp')
             	    title: "Please provide your email address and we will send a link to download the report (e.g: juandelacruz@deped.gov.ph)",
             	    inputType: 'email',
             	    callback: function (result) {
+            	    	
+            	    	if(!isValidEmailAddress(result))
+            	    		return false;
+//            	    	
             	        var sendTo=[];
             	        sendTo.push(result);
             	        var serArr=$("#exportForm").serializeArray();
@@ -96,16 +100,18 @@ angular.module('UserApp')
             	        obj.sendTo=sendTo;
             	        serArr[0].value=JSON.stringify(obj);
             	        
-            	        
-            	        
             	        $.post($("#exportForm").attr('action'),serArr);
             	        
-//            	        alert(angular.toJson($("#exportForm").serializeArray()));
-//            	        console.log($scope.datasetJson);
-//            	        $("#exportForm").submit();
+            	    
+            	    	
             	    }
             	});
             }
+            
+            function isValidEmailAddress(emailAddress) {
+                var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+                return pattern.test(emailAddress);
+            };
             
             function call()
             {
